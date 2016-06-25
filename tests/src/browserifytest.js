825e2d14eebd575b05d6bc131eea9424de2e5043
Daniel Hewes <daniel@lambdacreatives.com>
2016-06-25 15:53:36 -0700

    WIP, getting it almost working.

--- tests/src/browserifytest.js
+++ tests/src/browserifytest.js
@@ -1,8 +1,13 @@
 import $ from 'jquery';
 import jQuery from 'jquery';
-// export for others scripts to use
-window.$ = $;
-window.jQuery = jQuery;
+// // export for others scripts to use
+//window.$ = $;
+
+//window.jQuery = jQuery;
 import sharelinks from '../../js/sharelinks';
 
-$('.share').sharelinks();
+$(function() {
+  console.log(sharelinks()); // WTF, this returns jQuery
+
+  $('.share').sharelinks(); // Then this works?
+});

b918c609b5b69b63f803c6bd61ac8e678d0e179b
Daniel Hewes <daniel@creativedotdesign.com>
2021-03-31 19:03:36 -0700

    feat: Add build step.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const production = process.env.NODE_ENV === 'production' || false
+const path = require('path')
+
+module.exports = {
+  entry: './Sharelinks.js',
+  mode: 'production',
+  target: 'web',
+  output: {
+    filename: production ? 'Sharelinks.min.js' : 'Sharelinks.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Sharelinks',
+    globalObject: 'this',
+    libraryExport: 'default',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  }
+}

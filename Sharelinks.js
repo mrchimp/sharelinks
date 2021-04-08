0f7d63d5967cac3d83a33639b9dc2880b894e72d
Jake Gully <jake@maya.agency>
2021-04-08 21:54:35 +0100

    Code style fixes

--- Sharelinks.js
+++ Sharelinks.js
@@ -3,7 +3,7 @@
  * @param {String} suffix
  * @param {Element} element
  */
-function getAttributeValue (suffix, element) {
+function getAttributeValue(suffix, element) {
   const attribute = `data-${suffix}`;
 
   if (!element.hasAttribute(attribute)) {
@@ -19,42 +19,43 @@ let platforms = [
     href: 'whatsapp://send?text=%URL%',
     width: null,
     height: null,
-    sameWindow: true,
+    sameWindow: true
   },
   {
     name: 'facebook',
     href: 'https://www.facebook.com/sharer/sharer.php?u=%URL%',
     width: 400,
-    height: 500,
+    height: 500
   },
   {
     name: 'twitter',
     href: 'https://twitter.com/intent/tweet?text=%TITLE%+-+%URL%',
     width: 540,
-    height: 260,
+    height: 260
   },
   {
     name: 'pinterest',
-    href: 'http://pinterest.com/pin/create/button/?url=%URL%&description=%TITLE%&media=%IMAGE%',
+    href:
+      'http://pinterest.com/pin/create/button/?url=%URL%&description=%TITLE%&media=%IMAGE%',
     width: 520,
-    height: 570,
+    height: 570
   },
   {
     name: 'tumblr',
     href: 'http://www.tumblr.com/share/link?url=%URL%',
     width: 500,
-    height: 500,
+    height: 500
   },
   {
     name: 'linkedin',
     href: 'https://www.linkedin.com/sharing/share-offsite/?url=%URL%',
     width: 520,
-    height: 570,
-  },
+    height: 570
+  }
 ];
 
 class Sharelinks {
-  constructor (selector, options) {
+  constructor(selector, options) {
     if (options && 'platforms' in options) {
       platforms.push(...options.platforms);
     }
@@ -62,22 +63,20 @@ class Sharelinks {
     this.listenClick(selector);
   }
 
-  makeLink (platform, url, title, image) {
+  makeLink(platform, url, title, image) {
     return platform.href
       .replace('%URL%', encodeURIComponent(url).replace(/%20/g, '+'))
       .replace('%TITLE%', encodeURIComponent(title).replace(/%20/g, '+'))
       .replace('%IMAGE%', encodeURIComponent(image).replace(/%20/g, '+'));
   }
 
-  listenClick (selector) {
+  listenClick(selector) {
     document.querySelectorAll(selector).forEach((link) => {
-      link.addEventListener('click', (e) => {
-        this.onClick(e);
-      });
+      link.addEventListener('click', this.onClick);
     });
   }
 
-  findImage (elem) {
+  findImage(elem) {
     if (getAttributeValue('image', elem)) {
       return getAttributeValue('image', elem);
     }
@@ -89,35 +88,37 @@ class Sharelinks {
     }
   }
 
-  onClick (e) {
+  onClick(e) {
     // Left click only! Don't hijack middle click!
     if (e.which == 1) {
       e.preventDefault();
 
-      const elem = e.target,
-        platform = platforms.find((platform) => {
-          return platform.name === elem.dataset.platform;
-        }),
-        url = elem.getAttribute('href') || window.location.href;
+      const elem = e.target;
+      const platform = platforms.find((platform) => {
+        return platform.name === elem.dataset.platform;
+      });
+      const url = elem.getAttribute('href') || window.location.href;
 
       if (typeof platform === 'undefined') {
-        throw 'Sharelinks Error: Invalid data-platform: ' + getAttributeValue('platform', elem);
+        throw (
+          'Sharelinks Error: Invalid data-platform: ' +
+          getAttributeValue('platform', elem)
+        );
       }
 
-      const width = getAttributeValue('width', elem) || platform.width,
-        height = getAttributeValue('height', elem) || platform.height,
-        href = this.makeLink(
-          platform,
-          url,
-          getAttributeValue('title', elem) || document.title,
-          this.findImage(elem)
-        );
-
+      const width = getAttributeValue('width', elem) || platform.width;
+      const height = getAttributeValue('height', elem) || platform.height;
+      const href = this.makeLink(
+        platform,
+        url,
+        getAttributeValue('title', elem) || document.title,
+        this.findImage(elem)
+      );
       const event = new CustomEvent('share-link-clicked', {
         detail: {
           platform: elem.dataset.platform,
-          url: getAttributeValue('url', elem) || window.location.href,
-        },
+          url: getAttributeValue('url', elem) || window.location.href
+        }
       });
 
       elem.dispatchEvent(event);
